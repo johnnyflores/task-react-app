c6ed0b37058f5c08d08e763f6c3e049ac5dfee3d
Johnny <johnnyflores@live.ca>
2026-02-08 22:11:37 -0500

    Updated error page template

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,3 +1,4 @@
+import Logo from "@/components/Logo/Logo";
 import { Link, useLocation } from "react-router-dom";
 
 type ErrorPageProps = {
@@ -10,17 +11,24 @@ export default function ErrorPage({ status, message }: ErrorPageProps) {
   const path = location.pathname;
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center p-4">
-      <h1 className="text-6xl font-bold mb-4">{status || 404}</h1>
-      <p className="text-xl mb-2">
-        {message || `Oops! The page "${path}" does not exist.`}
-      </p>
-      <Link
-        to="/dashboard"
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mt-4"
-      >
-        Go to Dashboard
-      </Link>
+    <div className="grid min-h-svh ">
+      <div className="flex flex-col gap-4 p-6 md:p-10 md:pt-6">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Logo url="/" />
+        </div>
+        <div className="flex flex-col items-center justify-center h-screen text-center">
+          <h1 className="text-6xl font-bold mb-4">{status || 404}</h1>
+          <p className="text-xl mb-2">
+            {message || `Oops! The page "${path}" does not exist.`}
+          </p>
+          <Link
+            to="/dashboard"
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mt-4"
+          >
+            Go to Dashboard
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
